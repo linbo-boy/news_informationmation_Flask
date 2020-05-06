907a7c18ef49074469f2508605229c12293fa97e
linbo <13237425527@163.com>
2020-05-06 22:35:19 +0800

    项目的配置&数据库的配置

--- manage.py
+++ manage.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+
+class Config(object):
+    """工程配置信息"""
+    Debug = True
+    """为数据库添加配置"""
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/news_information'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
+pymysql.install_as_MySQLdb()
 
 
 @app.route('/')
@@ -9,4 +25,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
