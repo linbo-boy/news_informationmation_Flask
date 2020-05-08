8d9285f6fcf991c63a13cb2cdef80a51813cf66f
linbo <13237425527@163.com>
2020-05-08 14:54:43 +0800

    注册根路由&网站图标展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,14 @@
+from flask import render_template, current_app
+
 from . import index_blu
 from ... import redis_store
 
 
 @index_blu.route('/')
 def index():
-    # 向redis中保存一个值name lin
-    redis_store.set('name', 'linbo')
-    return 'index'
+    return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
