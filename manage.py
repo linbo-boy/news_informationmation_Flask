20f1a1ddf135e2cbea523a66014e8f2a6501b7f9
linbo <13237425527@163.com>
2020-05-08 12:32:19 +0800

    导入模型类&数据库迁移&测试数据导入

--- manage.py
+++ manage.py
@@ -1,9 +1,6 @@
-import logging
-
-from flask import session, current_app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import create_app, db
+from info import create_app, db, models
 
 # 通过指定的配置文件创建对应配置的app
 # create_app 就类似于工厂方法
