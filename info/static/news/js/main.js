b85e1aa1b2bba8cbcae5baf4ddc4b58dc7645352
linbo <13237425527@163.com>
2020-05-11 20:54:48 +0800

    注册的前端逻辑实现

--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -33,9 +33,17 @@ $(function(){
 
 
 	// 点击输入框，提示文字上移
-	$('.form_group').on('click focusin',function(){
-		$(this).children('.input_tip').animate({'top':-5,'font-size':12},'fast').siblings('input').focus().parent().addClass('hotline');
-	})
+	// $('.form_group').on('click focusin',function(){
+	// 	$(this).children('.input_tip').animate({'top':-5,'font-size':12},'fast').siblings('input').focus().parent().addClass('hotline');
+	// })
+    $('.form_group').on('click',function(){
+        $(this).children('input').focus()
+    })
+
+    $('.form_group input').on('focusin',function(){
+        $(this).siblings('.input_tip').animate({'top':-5,'font-size':12},'fast')
+        $(this).parent().addClass('hotline');
+    })
 
 	// 输入框失去焦点，如果输入框为空，则提示文字下移
 	$('.form_group input').on('blur focusout',function(){
@@ -143,8 +151,28 @@ $(function(){
             return;
         }
 
+		// 准备参数
+		var params = {
+		    "mobile": mobile,
+            "smscode": smscode,
+            "password": password,
+        }
         // 发起注册请求
-
+        $.ajax({
+            url:"/passport/register",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 代表注册成功
+                }else {
+                    // 代表注册失败
+                    $("#register-password-err").html(resp.errmsg)
+                    $("#register-password-err").show()
+                }
+            }
+        })
     })
 })
 
