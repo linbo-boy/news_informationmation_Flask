de81b2d3eeccb5b55a84da598cca1d6b51d18956
linbo <13237425527@163.com>
2020-05-12 20:32:01 +0800

    首页的排行列表新闻展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,7 +2,7 @@ from flask import render_template, current_app, session
 
 from . import index_blu
 from ... import redis_store
-from ...models import User
+from ...models import User, News
 
 
 @index_blu.route('/')
@@ -11,6 +11,8 @@ def index():
     1.如果用户已经登录，将当前登录用户的数据传到模板中，供模板显示
     :return:
     """
+    # 显示用户是否登录的逻辑
+    # 获取用户id
     user_id = session.get("user_id", None)
     user = None
     if user_id:
@@ -18,8 +20,21 @@ def index():
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.errno(e)
+
+    # 右侧新闻排行的逻辑
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc().limit(6))
+    except Exception as e:
+        current_app.logger.error(e)
+    news_dict_li = []
+    # 遍历对象列表，将对象的字典添加到字典列表中
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
+
     data = {
-        "user": user.to_dict() if user else None
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_li
     }
     return render_template('news/index.html', data=data)
 
