89dae481203cd25c9a18ca71a3ad112559ebe7a4
linbo <13237425527@163.com>
2020-05-13 21:01:18 +0800

    解决评论后立马点赞不显示的BUG

--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -182,6 +182,9 @@ $(function(){
                 success: function (resp) {
                     if (resp.errno == "0") {
                         var like_count = $this.attr('data-likecount')
+                        if (like_count == undefined) {
+                            like_count = 0
+                        }
                         // 更新点赞按钮图标
                         if (action == "add") {
                             like_count = parseInt(like_count) + 1
