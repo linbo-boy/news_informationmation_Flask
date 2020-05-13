2ebcc21ba046819a3f421602aefde5aabc21d688
linbo <13237425527@163.com>
2020-05-13 17:52:09 +0800

    创建个人中心模块&基类模板的抽取

--- info/__init__.py
+++ info/__init__.py
@@ -73,4 +73,7 @@ def create_app(config_name):
     from info.modules.news import news_blu
     app.register_blueprint(news_blu)
 
+    from info.modules.profile import profile_blu
+    app.register_blueprint(profile_blu)
+
     return app
---
+++ info/modules/profile/views.py
@@ -0,0 +1,24 @@
+from flask import render_template, redirect, g
+
+from info.modules.profile import profile_blu
+from info.utils.common import user_login_data
+
+
+@profile_blu.route('/info')
+@user_login_data
+def get_user_info():
+    """
+    获取用户信息
+    1. 获取到当前登录的用户模型
+    2. 返回模型中指定内容
+    :return:
+    """
+    user = g.user
+    if not user:
+        # 用户未登录，重定向到主页
+        return redirect('/')
+    data = {
+        "user": user.to_dict(),
+    }
+    # 渲染模板
+    return render_template("news/user.html", data=data)
