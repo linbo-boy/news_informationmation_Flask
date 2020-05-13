006709b0371502883cc130ac8895714772146c9f
linbo <13237425527@163.com>
2020-05-13 18:57:23 +0800

    基本资料设置BUG修改&前后端逻辑实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, g, request
-
+from flask import render_template, redirect, g, request, jsonify
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @profile_blu.route('/info')
@@ -29,8 +29,31 @@ def get_user_info():
 def base_info():
     """
     用户基本信息
+    1. 获取用户登录信息
+    2. 获取到传入参数
+    3. 更新并保存数据
+    4. 返回结果
     :return:
     """
+    # 1. 获取当前登录用户的信息
+    user = g.user
     if request.method == "GET":
         return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
     # 如果是POST，代表是修改用户数据
+    # 2. 获取到传入参数
+    data_dict = request.json
+    nick_name = data_dict.get("nick_name")
+    gender = data_dict.get("gender")
+    signature = data_dict.get("signature")
+    # 校验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+    if gender not in (['MAN', 'WOMAN']):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+    # 3. 更新并保存数据
+    user.nick_name = nick_name
+    user.gender = gender
+    user.signature = signature
+
+    # 4. 返回响应
+    return jsonify(errno=RET.OK, errmsg="更新成功")
