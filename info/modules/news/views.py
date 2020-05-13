0f1561a942c1e9f92610cd1d0d5a9af002f9809f
linbo <13237425527@163.com>
2020-05-13 14:06:02 +0800

    收藏和取消收藏后端逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,9 +1,10 @@
-from flask import render_template, current_app, session, g, abort
+from flask import render_template, current_app, session, g, abort, request, jsonify
 
 from info import constants
 from info.models import News, User
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @news_blu.route("/<int:news_id>")
@@ -50,3 +51,56 @@ def news_detail(news_id):
         "is_collected": is_collected
     }
     return render_template("news/detail.html", data=data)
+
+
+@news_blu.route("/news_collect", methord=["POST"])
+@user_login_data
+def collect_news():
+    """
+    新闻收藏
+    1.接受参数
+    2.判断参数
+    3.查询新闻并判断新闻是否存在
+    :return:
+    """
+    # 显示用户是否登录的逻辑
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1.接受参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    # 2.判断参数
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3.查询新闻并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库查询错误")
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到数据")
+
+    if action == "cancel_collect":
+        # 取消收藏
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    else:
+        # 4.进行收藏
+        if news not in user.collection_news:
+            # 添加到用户的新闻收藏列表
+            user.collection_news.append(news)
+
+        return jsonify(errno=RET.OK, errmsg="操作成功")
