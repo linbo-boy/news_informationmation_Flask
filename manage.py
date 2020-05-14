73aa1162a131b1818d9e867a67bb52820d8e0879
linbo <13237425527@163.com>
2020-05-14 13:33:41 +0800

    创建管理员用户

--- manage.py
+++ manage.py
@@ -4,12 +4,34 @@ from info import create_app, db, models
 
 # 通过指定的配置文件创建对应配置的app
 # create_app 就类似于工厂方法
+from info.models import User
+
 app = create_app('development')
 manager = Manager(app)
 # 将app与db关联
 Migrate(app, db)
 # 将数据库迁移命令添加到manager中
 manager.add_command('db', MigrateCommand)
+
+
+@manager.option('-n', '-name', dest='name')
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+    if not all([name, password]):
+        print("参数不足")
+
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+    print("添加成功")
 
 
 if __name__ == "__main__":
